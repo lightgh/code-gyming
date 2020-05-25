f6e571e9e0981035b969840e9a415b8571d821af
Light I Chinaka <chinakalight@googlemail.com>
2020-05-25 12:26:54 +0100

    node test commit

---
+++ src/com/chinakalight/datastructure/nodes/NodeTest.java
@@ -0,0 +1,11 @@
+package com.chinakalight.datastructure.nodes;
+
+/**
+ * @author - Chinaka .I. Light <ichinaka@byteworks.com.ng>
+ * Date: 5/25/2020
+ */
+public class NodeTest {
+    public static void main(String[] args) {
+        Node node = new Node();
+    }
+}
